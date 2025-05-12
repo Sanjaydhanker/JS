f73b472cc134b0707141b3b4a635057c70b8af1a
Sanjaydhanker <sanjaydhanker87@gmail.com>
2025-05-12 15:29:25 +0000

    Datatypes

---
+++ 01_Basic/01_Datatype.js
@@ -0,0 +1,34 @@
+"use strict";   //treat all js code as new version
+
+//alert(3 + 3)  //we are using node js not browser
+
+
+console.log(3 +
+     3)  //   
+
+
+     let name ="sanjay" 
+     let age =23
+     let isUser=false
+
+
+
+     // datatypes
+     //number
+     //bigint       for big numbers
+     //boolean      true false
+     //string
+     //null         stand alone value     //empty   //null is a object
+     //undefined    
+     //symbol        for unique
+
+
+
+     //object
+
+
+     console.log(typeof age);
+      
+     console.log(typeof null);  //object
+     console.log(typeof undefined);  //undefined
+     
