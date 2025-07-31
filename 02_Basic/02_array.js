a6cad65c28bb27eb68b4e55ab8d0a54d943256eb
Sanjaydhanker <sanjaydhanker87@gmail.com>
2025-07-31 05:33:52 +0000

    array

---
+++ 02_Basic/02_array.js
@@ -0,0 +1,34 @@
+const marvel = ["thor" , "Ironman" , "spiderman"]
+const dc = ["superman" , "flash" , "batman"]
+
+
+// marvel.push(dc)
+// console.log(marvel);
+// console.log(marvel[3][2]);
+
+// const allheros = marvel.concat(dc)
+
+// console.log(allheros);
+
+// const allNewHero = [...marvel, ...dc]
+// console.log(allNewHero);
+
+
+const myNewArr =  [1, 2, 3, [4, 5], 6, 7, [8, 9, [3, 2]]]
+
+const anotherArr= myNewArr.flat(3)
+
+// console.log(myNewArr);
+// console.log(anotherArr);
+
+// console.log(Array.isArray("sanjay"));    // to check array or not
+
+// console.log(Array.from("Sanjay"));       // to create a array
+
+let num1 = 100
+let num2 = 200
+let num3 = 300
+
+// console.log(Array.of(num1,num2,num3));      // many var ,string to one array
+
+
