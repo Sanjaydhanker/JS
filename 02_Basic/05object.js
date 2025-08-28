d817004a777dca49726607a4db53c7eb7a26f706
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2025-08-28 13:01:25 +0530

    object de structure

---
+++ 02_Basic/05object.js
@@ -0,0 +1,13 @@
+const user =  {
+    name:"sanjay",
+    age : 20,
+    lastname:"dhanker"
+}
+
+// de-structure
+
+// console.log(user.name);
+
+const {name} = user
+console.log(name);     // destructure
+
