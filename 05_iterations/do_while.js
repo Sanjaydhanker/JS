8ea10cff51bc4c39c9b3176f64a688b2a45e039c
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2025-09-09 18:38:11 +0530

    do while

---
+++ 05_iterations/do_while.js
@@ -0,0 +1,27 @@
+
+// let i = 1;
+// while (i <= 10) {
+
+//     console.log(i);
+//     i++
+    
+// }
+
+// whille with array
+
+// let myHero = ["shaktiman" , "Superman" , "krish"]
+// let myArr = 0
+// while (myArr < myHero.length) {
+//     console.log(myHero[myArr]);
+//     myArr  = myArr + 1
+    
+// }
+
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score = score + 1
+    
+} while (score <= 10);
