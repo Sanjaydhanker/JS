42990cdcb1946c3c9c15ffaf287c9ed47d3a9f0e
Sanjay Dhanker <sanjaydhanker87@gmail.com>
2025-09-12 11:53:33 +0530

    foreach

---
+++ 05_iterations/foreach.js
@@ -0,0 +1,45 @@
+const myHero = ["Shaktiman" , "Thor" , "Hulk" , "IronMan"]
+
+// myHero.forEach(function (item){
+// console.log(item);
+
+// })
+
+// myHero.forEach( (val) => {
+//     console.log(val);
+    
+// })
+
+// function myHeroFunc(value){
+//     console.log(value);
+    
+// }
+// myHero.forEach(myHeroFunc)
+
+
+// myHero.forEach((hero , index , arr) => {
+//     console.log(hero , index , arr);
+    
+// })
+
+
+// foreach for aob in Array
+
+const myArr = [
+    {
+    name:"sanjay",
+    lastName:"kumar"
+},
+{
+    name:"dev",
+    lastName:"kumar"
+},
+{
+    name:"harsh",
+    lastName:"kumar"
+}
+]
+myArr.forEach( (items) => {
+console.log(items.lastName);
+
+})
